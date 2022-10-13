07969e3f4b448f16a97abea0b64a49c8c47922fe
Justus <justus@justcoding.tech>
2022-10-13 15:14:30 +0200

    Removed LuckPerms

--- src/main/java/tech/justcoding/homburgplots/Main.java
+++ src/main/java/tech/justcoding/homburgplots/Main.java
@@ -1,35 +1,23 @@
 package tech.justcoding.homburgplots;
 
-import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import tech.justcoding.homburgplots.commands.PingCommand;
-import tech.justcoding.homburgplots.plots.PlotBossbar;
-import tech.justcoding.homburgplots.plots.PlotBuildCheck;
-import tech.justcoding.homburgplots.plots.PlotRegistrar;
-import tech.justcoding.homburgplots.plots.SavePlotCommand;
-import tech.justcoding.homburgplots.plots.SpawnPlotProtection;
+import tech.justcoding.homburgplots.plots.*;
 import tech.justcoding.homburgplots.recipes.PlotTeleportCompass;
 import tech.justcoding.homburgplots.recipes.RecipeManager;
 import tech.justcoding.homburgplots.teamselector.TeamSelector;
-import tech.justcoding.homburgplots.teamselector.TeamSelectorManager;
 import tech.justcoding.homburgplots.utils.Config;
 
 public final class Main extends JavaPlugin {
 
-    public static LuckPerms luckPerms = null;
     @Override
     public void onEnable() {
         // Plugin startup logic
         Config.registerConfiguration();
         PlotRegistrar.getPlotsFromConfig();
-        RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
-        if (provider != null) {
-            luckPerms = provider.getProvider();
-        }
         registerCommands();
         registerListeners();
         RecipeManager.registerAllRecipes();
